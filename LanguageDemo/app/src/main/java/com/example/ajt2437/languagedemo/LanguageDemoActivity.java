b2704ed3dc86e00b774c856fe8bfee1f69cfd7e5
Torres <Abe.Torres@athletics.utexas.edu>
2016-10-17 11:03:35 -0500

    first commit

---
+++ LanguageDemo/app/src/main/java/com/example/ajt2437/languagedemo/LanguageDemoActivity.java
@@ -0,0 +1,13 @@
+package com.example.ajt2437.languagedemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LanguageDemoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_language_demo);
+    }
+}
